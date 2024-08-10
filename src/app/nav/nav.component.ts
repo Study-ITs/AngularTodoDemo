f36bc38d4db2ce4538f7a53efb4de04c88540640
ItStudy <st2000study@gmail.com>
2024-08-10 19:31:21 +0900

    update CSS調整

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { ProjectService } from '../project.service';
 
 export interface Project {
   id: number;
@@ -14,13 +15,24 @@ export interface Project {
   standalone: true,
   imports: [CommonModule, MatListModule, MatIconModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
   @Input() projects: Project[] = [];
+  selectedProjectId: number | null = null;
+
   @Output() projectSelected = new EventEmitter<number>();
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projectService.selectedProjectId$.subscribe(projectId => {
+      this.selectedProjectId = projectId;
+    });
+  }
 
   selectProject(projectId: number): void {
     this.projectSelected.emit(projectId);
+    this.projectService.setSelectedProjectId(projectId); // プロジェクトIDを状態に設定
   }
 }
